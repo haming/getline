f3e09ce14f214cd543c26ea4e6e2037c7fefeffc
haoming <735008677@qq.com>
2018-05-07 18:21:36 +0800

    fix

--- node-src-getline/public/javascripts/index.js
+++ node-src-getline/public/javascripts/index.js
@@ -9,14 +9,19 @@ $(document).ready(function () {
                     line : $("#line").val()
                 }
                 $.post("/getText",text,function (val) {
+                    console.log("val");
                     console.log(val);
                     $("#showCode").text(val)
                 })
             })
         },
+
+        test :function () {
+            var mySwiper = new Swiper('.swiper-container', {
+                autoplay: 5000,//可选选项，自动滑动
+            })
+        }
     }
-
-
-
     vm.action();
+    vm.test();
 })
--- node-src-getline/routes/index.js
+++ node-src-getline/routes/index.js
@@ -1,31 +1,71 @@
 var express = require('express');
 var http = require('http');
-var fs = require('fs');
+// var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
 var router = express.Router();
-
+var url = "";
+var i = 0;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+/* 跨域 */
+router.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    // res.header("Content-Type", "application/json");
+    next();
+});
+
+router.get('/test', function(req, res, next) {
+    res.send("success")
 });
 
 router.post('/getText', function(req, ress, next) {
-  console.log(req.body.line)
-   // var STR = loadPage(req.body.line)
-   //  res.send(STR);
+    url = req.body.line;
    //  http://www.baidu.com
    // http://tuijian.hao123.com/hotrank
-
-    http.get(req.body.line,function (res) {
+    if(url.search("http://") == -1){ //非http://
+        if(url.search("https://") != -1){
+            url = url.replace("https","http")
+        }else{
+            url = "http://" + url;
+        }
+    }
+    console.log(url)
+    http.get(url,function (res) {
         var html = '';
 
         res.on("data",function (d) {
             html += d;
         })
-        res.on('end',function(){
-            ress.send(html)
+        res.on('end',function() {
+            // ress.send(html)
+
+            var $ = cheerio.load(html); //采用cheerio模块解析html
+
+            var time = $('.article-info a:first-child').next().text().trim();
+
+            var news_item = {
+                //获取文章的标题
+                title: $('div.article-title a').text().trim(),
+                //获取文章发布的时间
+                Time: time,
+                //获取当前文章的url
+                link: "http://www.ss.pku.edu.cn" + $("div.article-title a").attr('href'),
+                //获取供稿单位
+                author: $('[title=供稿]').text().trim(),
+                //i是用来判断获取了多少篇文章
+                i: i = i + 1,
+            }
+
+            console.log(news_item);
+
+            ress.send(news_item)
         })
     })
-
 });
 
 
